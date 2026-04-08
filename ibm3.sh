77d03fc3a2240f3a3425eb38dc3dda7c77d7f3fd
Pedro Angel Miranda <41977683+peterwtf@users.noreply.github.com>
2026-04-08 16:10:44 -0600

    data
    data

--- ibm3.sh
+++ ibm3.sh
@@ -6,7 +6,7 @@ if [ -n "$RESULT" ]; then
     echo "Informix encontrado en este servidor"
     echo "Ruta(s):"
     echo "$RESULT"
-    echo ""   # salto de línea limpio
+    echo ""  
 fi
 
 RESULT=$(find / -name dmts64 2>/dev/null)
@@ -15,5 +15,14 @@ if [ -n "$RESULT" ]; then
     echo "IIDR encontrado en este servidor"
     echo "Ruta(s):"
     echo "$RESULT"
-    echo ""   # salto de línea limpio
+    echo "" 
+fi
+
+RESULT=$(find / -name wsadmin.sh 2>/dev/null)
+
+if [ -n "$RESULT" ]; then
+    echo "WAS encontrado en este servidor"
+    echo "Ruta(s):"
+    echo "$RESULT"
+    echo ""  
 fi
