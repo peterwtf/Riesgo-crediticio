d8d4ec092719131f15be369c3afef3da23f1b25b
Pedro Angel Miranda <41977683+peterwtf@users.noreply.github.com>
2026-04-08 10:04:26 -0600

    data
    data

---
+++ ibm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+RESULT=$(find / -name oninit 2>/dev/null)
+
+if [ -n "$RESULT" ]; then
+    echo "Informix encontrado en este servidor"
+    echo "Ruta(s):"
+    echo "$RESULT"
+else
+    echo "No hay Informix encontrado en este servidor"
+fi
