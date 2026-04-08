3e514631ae7f98e42ca02bfcc5f3561a2d448ae9
Pedro Angel Miranda <41977683+peterwtf@users.noreply.github.com>
2026-04-08 16:05:55 -0600

    data
    data

---
+++ ibm3.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+RESULT=$(find / -name oninit 2>/dev/null)
+
+if [ -n "$RESULT" ]; then
+    echo "Informix encontrado en este servidor"
+    echo "Ruta(s):"
+    echo "$RESULT"
+    echo ""   # salto de línea limpio
+fi
+
+RESULT=$(find / -name dmts64 2>/dev/null)
+
+if [ -n "$RESULT" ]; then
+    echo "IIDR encontrado en este servidor"
+    echo "Ruta(s):"
+    echo "$RESULT"
+    echo ""   # salto de línea limpio
+fi
